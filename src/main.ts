c282a8eeb21230925a66a8527503939087454d48
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-04 01:11:30 -0300

    Feature: add api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { apiDocSetup } from './shared/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix(process.env.API_PREFIX ?? 'api');
+
+  apiDocSetup(app);
+
+  await app.listen(
+    process.env.APP_PORT ?? 3000,
+    process.env.APP_HOST ?? '0.0.0.0',
+  );
 }
 bootstrap();
---
+++ src/shared/config/swagger.config.ts
@@ -0,0 +1,71 @@
+import {
+  DocumentBuilder,
+  SwaggerModule,
+  ApiOperation,
+  ApiParam,
+  ApiOkResponse,
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+  ApiNotFoundResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
+import { INestApplication, applyDecorators } from '@nestjs/common';
+import { patchNestJsSwagger } from 'nestjs-zod';
+import { ConfigurationService } from '@src/shared/services/system/system.service'; // Importando o service
+
+export function apiDocSetup(app: INestApplication) {
+  patchNestJsSwagger();
+
+  const configService = app.get(ConfigurationService);
+
+  const config = new DocumentBuilder()
+    .setTitle(configService.getAppName())
+    .setDescription(configService.getAppDescription())
+    .setVersion(configService.getAppVersion())
+    .setContact(
+      configService.getAppAuthor().name,
+      configService.getAppAuthor().url,
+      configService.getAppAuthor().email,
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(`${process.env.API_PREFIX}/doc`, app, document, {
+    customSiteTitle: configService.getAppName(),
+    jsonDocumentUrl: `${process.env.API_PREFIX}/doc/json`,
+  });
+}
+
+export function apiDocGenerator(paramName?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const summary = propertyKey
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, (str) => str.toUpperCase());
+
+    const decorators = [
+      ApiOperation({ summary }),
+      ApiOkResponse(),
+      ApiBadRequestResponse(),
+      ApiUnauthorizedResponse(),
+      ApiNotFoundResponse(),
+      ApiInternalServerErrorResponse(),
+    ];
+
+    if (paramName) {
+      decorators.push(
+        ApiParam({
+          name: paramName,
+          example: 1,
+          description: `ID do ${summary.toLowerCase()}`,
+        }),
+      );
+    }
+
+    applyDecorators(...decorators)(target, propertyKey, descriptor);
+  };
+}
