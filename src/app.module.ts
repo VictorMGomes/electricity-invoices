11d40ac7b8161770032fdcb10035eed86818b17d
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-04 01:15:20 -0300

    Feature: integrate UsersModule and ConfigModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaService } from '@src/shared/services/prisma/prisma.service';
+import { ConfigModule } from '@src/shared/services/system/system.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
