a9e352b8d03b40f84a00a39411f26c44363b622c
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-05 19:06:28 -0300

    Feature: add EnergyBillsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { EnergyBillsModule } from './modules/energy-bills/energy-bills.module';
 import { PrismaService } from '@src/shared/services/prisma/prisma.service';
 import { ConfigModule } from '@src/shared/services/system/system.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule],
+  imports: [UsersModule, ConfigModule, EnergyBillsModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
