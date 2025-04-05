ad095ac9aa50b380268ae431e719d6eaaecaa93b
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-05 03:34:04 -0300

    Feature: add basic pages

---
+++ src/resources/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the About Page</h1>
+      <p>This is the starting point of your application.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
