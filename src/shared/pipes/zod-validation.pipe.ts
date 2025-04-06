80c7a8e65ae78219ef1f7ade9a8e6447fe34d2b3
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 04:47:46 -0300

    Feature: add ZodValidationPipe for schema validation in requests

---
+++ src/shared/pipes/zod-validation.pipe.ts
@@ -0,0 +1,16 @@
+// src/common/pipes/zod-validation.pipe.ts
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { ZodSchema } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform<T>(value: unknown): T {
+    const result = this.schema.safeParse(value);
+    if (!result.success) {
+      throw new BadRequestException(result.error.format());
+    }
+    return result.data as T;
+  }
+}
