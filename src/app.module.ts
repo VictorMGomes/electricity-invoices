c6be3827427aa0a47c9cce10ee60791a417e4307
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 04:46:59 -0300

    Fix: replace EnergyBillsModule with BillingModule in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
-import { EnergyBillsModule } from './modules/energy-bills/energy-bills.module';
+import { BillingModule } from './modules/billings/billing.module';
 import { PrismaService } from '@src/shared/services/prisma/prisma.service';
 import { ConfigModule } from '@src/shared/services/system/system.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule, EnergyBillsModule],
+  imports: [UsersModule, ConfigModule, BillingModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
