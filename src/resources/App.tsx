42f58b63c7e292d090e80fdc179f8bdcf0783ccc
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 23:56:53 -0300

    Feature: refactor App component to use dynamic routing with available pages

--- src/resources/App.tsx
+++ src/resources/App.tsx
@@ -1,26 +1,42 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HeadManager } from './components/HeadManager';
 import AppBar from './components/AppBar';
-import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import BillPage from './pages/BIllPage';
+import ClientPage from './pages/ClientPage';
+import { getAvailablePages } from './constants/availablePages';
+import { useTranslation } from 'react-i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/index.css';
 import './config/i18n.ts';
+import AuthorCreditBar from './components/AuthorCreditBar.tsx';
+import UploadPage from './pages/UploadPage.tsx';
+import DashboardPage from './pages/DashboardPage.tsx';
+
+const componentMap: Record<string, React.ReactNode> = {
+  DashboardPage: <DashboardPage />,
+  AboutPage: <AboutPage />,
+  BillPage: <BillPage />,
+  ClientPage: <ClientPage />,
+  UploadPage: <UploadPage />,
+};
 
 function App() {
+  const { t } = useTranslation();
+  const availablePages = getAvailablePages(t);
+
   return (
-    <>
-      <BrowserRouter>
-        <HeadManager />
-        <AppBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <HeadManager />
+      <AuthorCreditBar />
+      <AppBar />
+      <Routes>
+        {availablePages.map(({ path, component }) => (
+          <Route key={path} path={path} element={componentMap[component]} />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
