48c44f4592b46aaf3ca101d9f8baf47a5d36cbb5
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-07 01:44:20 -0300

    Refactor: update upload titles and messages in localization files for consistency

--- src/resources/pages/UploadPage.tsx
+++ src/resources/pages/UploadPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Container, Card } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import FileUpload from '../components/FileUpload';
 
 export default function BillPage() {
+  const { t } = useTranslation();
   return (
     <Container className="my-5">
       <Card className="mb-4">
         <Card.Body>
           <Card.Title as="h5" className="mb-3">
-            Enviar Arquivo PDF
+            {t('general.upload-title')}
           </Card.Title>
           <FileUpload />
         </Card.Body>
