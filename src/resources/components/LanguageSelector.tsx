1b7dfea339f3a7cd638fb4d6b3d2a29ab0fea5ed
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 23:58:18 -0300

    Refactor: enhance LanguageSelector with icon and styling improvements

--- src/resources/components/LanguageSelector.tsx
+++ src/resources/components/LanguageSelector.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 import i18n from 'i18next';
+import { FaGlobe } from 'react-icons/fa6';
 
 function LanguageSelector() {
   const { t } = useTranslation();
@@ -20,17 +21,24 @@ function LanguageSelector() {
   };
 
   return (
-    <Dropdown onSelect={handleSelect}>
-      <Dropdown.Toggle variant="light" size="sm" id="language-selector">
-        🌐 {LANGUAGES[currentLanguage] ?? currentLanguage}
+    <Dropdown onSelect={handleSelect} className="me-2">
+      <Dropdown.Toggle
+        variant="outline-secondary"
+        size="sm"
+        id="language-selector"
+        className="min-width-dropdown text-start"
+      >
+        <FaGlobe className="me-2" />
+        {LANGUAGES[currentLanguage] ?? currentLanguage}
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
+      <Dropdown.Menu className="text-center w-100">
         {Object.entries(LANGUAGES).map(([code, label]) => (
           <Dropdown.Item
             key={code}
             eventKey={code}
             active={currentLanguage === code}
+            className="d-flex justify-content-center"
           >
             {label}
           </Dropdown.Item>
