f05117d09d32917022cf8c797db31fceb41b2b82
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 23:55:49 -0300

    Feature: wrap App component with ThemeProvider and initialize theme

--- src/resources/main.tsx
+++ src/resources/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from './components/ThemeProvider.tsx';
+import { initTheme } from './services/themeService';
+
+initTheme();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
